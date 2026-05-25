dcac2361a803ba2382e352edecacefc080f51c68
heuri0419 <ruihu2026@gmail.com>
2026-05-25 05:55:26 -0400

    Delete mobile-cache-mvp directory
    稍微修改下文件位置

--- mobile-cache-mvp/constants.js
+++
@@ -1,6 +0,0 @@
-export const EDGE_FUNCTION_NAME = "run-script";
-
-export const STORAGE_KEYS = {
-  content: "mobile-cache-mvp.content-db.v4",
-  user: "mobile-cache-mvp.user-db.v4",
-};
--- mobile-cache-mvp/utils.js
+++
@@ -1,3 +0,0 @@
-export function structuredCopy(value) {
-  return JSON.parse(JSON.stringify(value));
-}
